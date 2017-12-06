cb699c0547d3cf938e903632a00f7140bc96499c
water <water@10.0.2.15>
2017-12-06 18:34:19 +0100

    Ajout du chiffrage de vigenere

--- src/encryption/application/controller/algorithm/encryption/Vigenere.java
+++ src/encryption/application/controller/algorithm/encryption/Vigenere.java
@@ -7,6 +7,8 @@ package encryption.application.controller.algorithm.encryption;
 
 import encryption.application.controller.algorithm.AEncryption;
 import encryption.application.controller.algorithm.EncryptionDecorator;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -17,12 +19,41 @@ public class Vigenere extends AEncryption {
     public Vigenere(EncryptionDecorator encrypt) {
         algo = encrypt;
     }
+    
+    private List stringToIntList(String string){
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0; i<string.length(); i++){
+            result.add((int) string.charAt(i));
+        }
+        return result;
+    }
+    
+    private String intListToString(List<Integer> list){
+        String result = new String();
+        for(int c :list){
+            result += (char) c;
+        }
+        
+        return result;
+    }
 
     @Override
     public void encrypt() {
         algo.encrypt();
         System.out.println("Vigenere Encrypt");
-      
+        List<Integer> value = stringToIntList(algo.getValue());
+        int valueLength = value.size();
+        List<Integer> key = stringToIntList(algo.getKey());
+        int keyLength = key.size();
+        
+        List<Integer> crypt = new ArrayList<>();
+        
+        for (int i = 0; i < valueLength; i++){
+            int res = (value.get(i)+ key.get(i%keyLength)-64)%94 +32;
+            crypt.add(res);
+        }
+        String cypher = intListToString(crypt);
+        this.setValue(cypher);
     }
 
     @Override
