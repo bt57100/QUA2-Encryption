73543a3ecbe23901e606a5cd93b4fc036de2ed7c
bt57100 <bt57100@users.noreply.github.com>
2017-12-08 11:22:08 +0100

    Remove unused import Label

--- src/encryption/application/controller/EncryptionController.java
+++ src/encryption/application/controller/EncryptionController.java
@@ -15,7 +15,6 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
